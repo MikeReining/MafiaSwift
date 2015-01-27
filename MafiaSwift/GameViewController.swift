4e29c5eee70a7fa22313f5d2b9408800e154c537
Mike Reining <emailmike@gmail.com>
2015-01-26 23:03:04 -0800

    Game fully playable as Sherif.  Next: Have to implement mafia game

--- MafiaSwift/GameViewController.swift
+++ MafiaSwift/GameViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class GameViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout  {
     var model: Model?
+    var citizensCount: Int = 0
     
+    @IBOutlet weak var citizensLeftLabel: UILabel!
     private let reuseIdentifier = "Cell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        citizensCount = model!.players.count - 1
+        citizensLeftLabel.text = "Citizens Left: \(citizensCount)"
+
     }
     
     
@@ -34,6 +38,30 @@ class GameViewController: UIViewController, UICollectionViewDataSource, UICollec
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
         println("\(indexPath.row)")
+        let cell = collectionView.cellForItemAtIndexPath(indexPath)
+
+        if model!.players[indexPath.row] == 1 {
+            println("You found the mafia!")
+            cell?.backgroundColor = UIColor.greenColor()
+
+            let alert = UIAlertController(title: "You got the mafia!", message: "Success.  Mafia found just in time!", preferredStyle: .Alert)
+            let action = UIAlertAction(title: "Awesome!", style: .Default, handler: nil)
+            alert.addAction(action)
+            presentViewController(alert, animated: true, completion: nil)
+            
+        } else {
+            cell?.backgroundColor = UIColor.redColor()
+            citizensCount -= 1
+            citizensLeftLabel.text = "Citizens Left: \(citizensCount)"
+
+        }
+        if citizensCount == 0 {
+            let alert = UIAlertController(title: "Game Over!", message: "The Mafia has killed everyone!", preferredStyle: .Alert)
+            let action = UIAlertAction(title: ":(", style: .Default, handler: nil)
+            alert.addAction(action)
+            presentViewController(alert, animated: true, completion: nil)
+        }
+        
     }
 
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
